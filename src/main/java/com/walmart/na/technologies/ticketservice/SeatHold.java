01c567686ff17b5c54d7230a46b7265704ac26af
jac7mc <jac7mc@virginia.edu>
2018-03-30 00:14:58 -0400

    Initial project commit

---
+++ src/main/java/com/walmart/na/technologies/ticketservice/SeatHold.java
@@ -0,0 +1,7 @@
+package com.walmart.na.technologies.ticketservice;
+
+/**
+ * Created by Jeff on 3/29/2018.
+ */
+public class SeatHold {
+}
---
+++ src/main/java/com/walmart/na/technologies/ticketservice/Venue.java
@@ -0,0 +1,24 @@
+package com.walmart.na.technologies.ticketservice;
+
+/**
+ * Created by Jeff on 3/29/2018.
+ */
+public class Venue implements TicketService{
+    public int numSeatsAvailable(){
+        return 0;
+    };
+
+    public SeatHold findAndHoldSeats(int numSeats, String customerEmail){
+        return null;
+    }
+
+    public String reserveSeats(int seatHoldId, String customerEmail){
+        return "";
+    }
+
+
+
+
+
+
+}
